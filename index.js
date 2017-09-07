2bc37d2b64d338e329af742b0a68f6df53a7a0cb
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-07 23:03:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arrangments = [];
+  for (i = 0; i < instruments.length; i++) {
+    for (j = 0; j < musicians.length; j++) {
+      arrangments.push(`${musicians[j]} plays ${instruments[i]}.`)
+    }
+  }
+  return arrangments
+}
+
+function johnLennonFacts(facts) {
+  newFacts = [];
+  for (i = 0; i < facts.length) {
+    newFacts.push(facts[i] + '!!!')
+  }
+  return newFacts
+}
